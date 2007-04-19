180f6c5e0813eedf3c2e9ddb5c049c61652502c4
Tania Tudorache <tudorache@users.noreply.github.com>
2007-04-19 23:05:06 +0000

    optimization and added method

--- src/edu/stanford/smi/protege/collab/annotation/gui/panel/AllAnnotationsPanel.java
+++ src/edu/stanford/smi/protege/collab/annotation/gui/panel/AllAnnotationsPanel.java
@@ -3,6 +3,9 @@ package edu.stanford.smi.protege.collab.annotation.gui.panel;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import javax.swing.Icon;
+
+import edu.stanford.smi.protege.collab.annotation.gui.AnnotationsIcons;
 import edu.stanford.smi.protege.collab.annotation.tree.AnnotationsTreeRoot;
 import edu.stanford.smi.protege.collab.annotation.tree.filter.TreeFilter;
 import edu.stanford.smi.protege.collab.annotation.tree.filter.UnsatisfiableFilter;
@@ -18,16 +21,19 @@ import edu.stanford.smi.protegex.server_changes.model.generated.Change;
 public class AllAnnotationsPanel extends AnnotationsTabPanel {
 	
 	public AllAnnotationsPanel(KnowledgeBase kb) {
-		super(kb, "All (C & OC)");
+		super(kb, "All (Ann. & Chg.)");
 	}
 	
 
 	public void refreshDisplay() {
 		if (getCurrentInstance() == null) {
+			getLabeledComponent().setHeaderLabel("Annotations and Changes (nothing selected)");
 			getAnnotationsTree().setRoot(null);
 			repaint();
 			return;
 		}
+		
+		getLabeledComponent().setHeaderLabel("Annotations and Changes on " + getCurrentInstance().getBrowserText());
 		
 		Collection<Change> changeAnnotationsRoots = ChangeOntologyUtil.getTopLevelChangeInstances(getCurrentInstance());
 		Collection<Annotation> ontologyCompAnnotationsRoots = ChangeOntologyUtil.getTopLevelAnnotationInstances(getCurrentInstance());
@@ -47,16 +53,18 @@ public class AllAnnotationsPanel extends AnnotationsTabPanel {
 		AnnotationsTreeRoot root = new AnnotationsTreeRoot(filteredRoots, filter);
 		
 		getAnnotationsTree().setRoot(root);
-		
-		root.reload();
-		
-		getAnnotationsTree().setSelectionRow(0);		
 	}	
 	
 	@Override
 	protected void onCreateAnnotation() {
 		System.out.println("Create");
 		
+	}
+	
+	@Override
+	public Icon getIcon() {
+		//return AnnotationsIcons.getOntologyAnnotationAndChangeIcon();
+		return null;
 	}
 	
 	
