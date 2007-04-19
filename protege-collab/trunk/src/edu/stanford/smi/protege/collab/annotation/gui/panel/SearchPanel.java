8e0084f664ad7329145316d2a2010e6089bed840
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-04-19 23:14:59 +0000

    bug fix

--- protege-collab/trunk/src/edu/stanford/smi/protege/collab/annotation/gui/panel/SearchPanel.java
+++ protege-collab/trunk/src/edu/stanford/smi/protege/collab/annotation/gui/panel/SearchPanel.java
@@ -1,24 +1,40 @@
 package edu.stanford.smi.protege.collab.annotation.gui.panel;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
+import java.util.List;
 
+import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 
+import edu.stanford.smi.protege.collab.annotation.gui.AnnotationsIcons;
+import edu.stanford.smi.protege.collab.annotation.tree.AnnotationsTreeRoot;
+import edu.stanford.smi.protege.collab.annotation.tree.filter.TreeFilter;
+import edu.stanford.smi.protege.collab.annotation.tree.filter.UnsatisfiableFilter;
+import edu.stanford.smi.protege.collab.annotation.tree.gui.ComplexFilterComponent;
+import edu.stanford.smi.protege.collab.changes.ChangeOntologyUtil;
 import edu.stanford.smi.protege.model.KnowledgeBase;
-import edu.stanford.smi.protege.util.ComponentFactory;
+import edu.stanford.smi.protege.resource.Icons;
+import edu.stanford.smi.protege.resource.ResourceKey;
 import edu.stanford.smi.protege.util.LabeledComponent;
 import edu.stanford.smi.protege.util.SelectableContainer;
+import edu.stanford.smi.protegex.server_changes.model.generated.Annotation;
+import edu.stanford.smi.protegex.server_changes.model.generated.Change;
 
 public class SearchPanel extends AnnotationsTabPanel {
+	private ComplexFilterComponent complexFilterComp;
+	private TreeFilter complexFilter;
 
 	public SearchPanel(KnowledgeBase kb) {
-		super(kb, "Search Panel");
+		super(kb, "Search");
 		
 		fixGUI();
 	}
@@ -29,26 +45,48 @@ public class SearchPanel extends AnnotationsTabPanel {
 				
 		Collection headerButtonCollection = labledComponent.getHeaderButtons(); 
 		for (int i=0; i < headerButtonCollection.size(); i++) {			
-			labledComponent.removeHeaderButton(i);
+			labledComponent.removeHeaderButton(0);
 		}
 		
+		labledComponent.addHeaderButton(getViewAction());
 		labledComponent.setHeaderComponent(null);
 		labledComponent.setHeaderLabel("Search Results");
+		
 		
 		SelectableContainer parent = (SelectableContainer) labledComponent.getParent();
 		
 		parent.remove(labledComponent);
 		parent.remove(getToolbar());
+
+		complexFilterComp = new ComplexFilterComponent(getKnowledgeBase());
 		
-		LabeledComponent searchPanel = new LabeledComponent("Search", null, true);
-		searchPanel.setBackground(Color.GREEN);
+		LabeledComponent searchPanel = new LabeledComponent("Search", complexFilterComp.getValueComponent());
+		JButton searchButton = new JButton("Search");
+		searchButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				onSearch();				
+			}			
+		});		
+		JPanel buttonPanel = new JPanel(new GridBagLayout());
+		buttonPanel.add(searchButton);		
+		searchPanel.setFooterComponent(buttonPanel);
 		
-		JSplitPane topBottomSplitPane = ComponentFactory.createTopBottomSplitPane(searchPanel, labledComponent);		
-		labledComponent.setPreferredSize(new Dimension(100, 200));
-		topBottomSplitPane.setDividerLocation(150 + topBottomSplitPane.getInsets().bottom);
+		JScrollPane searchScrollPane = new JScrollPane(searchPanel);		
 		
-		parent.add(topBottomSplitPane, BorderLayout.CENTER);
+		JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, searchScrollPane, labledComponent);
+		Dimension nullDim = new Dimension(0, 0);
+		searchScrollPane.setMinimumSize(nullDim);
+		labledComponent.setMinimumSize(nullDim);		
+		splitPane.setDividerLocation(175 + splitPane.getInsets().bottom);		
+		splitPane.setOneTouchExpandable(true);
+
+		parent.add(splitPane, BorderLayout.CENTER);
 		
+	}
+
+	protected void onSearch() {
+		complexFilter = complexFilterComp.getComplexFilter();
+		refreshDisplayAfterSearch();		
 	}
 
 	@Override
@@ -58,8 +96,34 @@ public class SearchPanel extends AnnotationsTabPanel {
 
 	@Override
 	public void refreshDisplay() {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stub		
+	}
+	
+	private void refreshDisplayAfterSearch() {		
+		Collection<Annotation> annotationsRoots = ChangeOntologyUtil.getAnnotationInstances(getKnowledgeBase());
+		Collection<Change> changeAnnotationsRoots = ChangeOntologyUtil.getChangeInstances(getKnowledgeBase());
+		
+		List allRoots = new ArrayList(annotationsRoots);
+		allRoots.addAll(changeAnnotationsRoots);
+		
+		Collection filteredRoots = ChangeOntologyUtil.getFilteredCollection(allRoots, complexFilter);
+		
+		//Collections.sort(filteredRoots, new AnnotationCreationComparator());
+		
+		//hack, reimplement later
+		TreeFilter filter = complexFilter;
+		
+		if (filter != null) {
+			filter = new UnsatisfiableFilter();
+		}
+		AnnotationsTreeRoot root = new AnnotationsTreeRoot(filteredRoots, filter);
+		
+		getAnnotationsTree().setRoot(root);
+	}
+	
+	@Override
+	public Icon getIcon() {	
+		return Icons.getIcon(new ResourceKey("object.search"));
 	}
 
 }
