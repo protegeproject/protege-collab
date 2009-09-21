02467cb497568d43796fa112b77a57d14f69ad43
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2009-09-21 06:29:48 +0000

    added getAnnotationCount

--- protege-collab/trunk/src/edu/stanford/smi/protege/collab/changes/ChAOUtil.java
+++ protege-collab/trunk/src/edu/stanford/smi/protege/collab/changes/ChAOUtil.java
@@ -37,147 +37,144 @@ import edu.stanford.smi.protege.util.LazyTreeNode;
 import edu.stanford.smi.protege.util.Log;
 import edu.stanford.smi.protegex.server_changes.GetAnnotationProjectName;
 
-
 /**
  * @author Tania Tudorache <tudorache@stanford.edu>
- *
+ * 
  */
 public class ChAOUtil {
 
+    public static Ontology_Component getOntologyComponent(Frame frame) {
+        return OntologyComponentCache.getOntologyComponent(frame);
+    }
 
-	public static Ontology_Component getOntologyComponent(Frame frame) {
-		return OntologyComponentCache.getOntologyComponent(frame);
-	}
+    public static Ontology_Component getOntologyComponent(Frame frame, boolean create) {
+        return OntologyComponentCache.getOntologyComponent(frame, create);
+    }
 
-	public static Ontology_Component getOntologyComponent(Frame frame, boolean create) {
-		return OntologyComponentCache.getOntologyComponent(frame, create);
-	}
+    static Collection<Annotation> getAnnotationInstances(Frame frame) {
+        ArrayList<Annotation> annotInstances = new ArrayList<Annotation>();
 
+        Ontology_Component ontologyComponent = getOntologyComponent(frame);
 
-	static Collection<Annotation> getAnnotationInstances(Frame frame) {
-		ArrayList<Annotation> annotInstances = new ArrayList<Annotation>();
+        if (ontologyComponent != null) {
+            annotInstances.addAll(ontologyComponent.getAssociatedAnnotations());
+        }
 
-		Ontology_Component ontologyComponent = getOntologyComponent(frame);
+        return annotInstances;
+    }
 
-		if (ontologyComponent != null) {
-			annotInstances.addAll(ontologyComponent.getAssociatedAnnotations());
-		}
+    public static Collection<Change> getChanges(Frame frame) {
+        ArrayList<Change> changeInstances = new ArrayList<Change>();
 
-		return annotInstances;
-	}
+        Ontology_Component ontologyComponent = getOntologyComponent(frame);
 
+        if (ontologyComponent != null) {
+            changeInstances.addAll(ontologyComponent.getChanges());
+        }
 
-	public static Collection<Change> getChanges(Frame frame) {
-		ArrayList<Change> changeInstances = new ArrayList<Change>();
+        return changeInstances;
+    }
 
-		Ontology_Component ontologyComponent = getOntologyComponent(frame);
+    public static Collection<Change> getTopLevelChanges(Frame frame) {
+        Collection<Change> allChanges = getChanges(frame);
+        if (allChanges == null || allChanges.size() == 0) {
+            return allChanges;
+        }
 
-		if (ontologyComponent != null) {
-			changeInstances.addAll(ontologyComponent.getChanges());
-		}
+        Collection<Change> toRemove = new ArrayList<Change>();
+        for (Change changeInstance : allChanges) {
+            Composite_Change cc = OntologyJavaMappingUtil.as(changeInstance, Composite_Change.class);
+            if (cc != null) {
+                toRemove.addAll(cc.getSubChanges());
+            }
+        }
+        allChanges.removeAll(toRemove);
+        return allChanges;
+    }
 
-		return changeInstances;
-	}
+    public static Collection<Annotation> getTopLevelAnnotationInstances(Frame frame) {
+        Collection<Annotation> allAnnotations = getAnnotationInstances(frame);
+        if (allAnnotations == null) {
+            return null;
+        }
 
+        Collection<Annotation> toRemove = new ArrayList<Annotation>();
+        for (Annotation annotInstance : allAnnotations) {
+            toRemove.addAll(annotInstance.getAssociatedAnnotations());
+        }
+        allAnnotations.removeAll(toRemove);
+        return allAnnotations;
+    }
 
-	public static Collection<Change> getTopLevelChanges(Frame frame) {
-		Collection<Change> allChanges = getChanges(frame);
-		if (allChanges == null || allChanges.size() == 0) {
-			return allChanges;
-		}
+    public static Collection<Annotation> getTopLevelAnnotationInstances(KnowledgeBase kb) {
+        Collection<Annotation> allAnnotations = getAnnotations(kb);
+        if (allAnnotations == null) {
+            return null;
+        }
 
-		Collection<Change> toRemove = new ArrayList<Change>();
-		for (Change changeInstance : allChanges) {
-			Composite_Change cc = OntologyJavaMappingUtil.as(changeInstance, Composite_Change.class);
-			if (cc != null) {
-				toRemove.addAll(cc.getSubChanges());
-			}
-		}
-		allChanges.removeAll(toRemove);
-		return allChanges;
-	}
+        Collection<Annotation> toRemove = new ArrayList<Annotation>();
+        for (Annotation annotInstance : allAnnotations) {
+            toRemove.addAll(annotInstance.getAssociatedAnnotations());
+        }
+        allAnnotations.removeAll(toRemove);
+        return allAnnotations;
+    }
 
-	public static Collection<Annotation> getTopLevelAnnotationInstances(Frame frame) {
-		Collection<Annotation> allAnnotations = getAnnotationInstances(frame);
-		if (allAnnotations == null) {
-			return null;
-		}
+    @SuppressWarnings("unchecked")
+    public static Collection<Annotation> getAnnotations(KnowledgeBase kb) {
+        AnnotationFactory factory = new AnnotationFactory(getChangesKb(kb));
+        return factory.getAllAnnotationObjects(true);
+    }
 
-		Collection<Annotation> toRemove = new ArrayList<Annotation>();
-		for (Annotation annotInstance : allAnnotations) {
-			toRemove.addAll(annotInstance.getAssociatedAnnotations());
-		}
-		allAnnotations.removeAll(toRemove);
-		return allAnnotations;
-	}
+    @SuppressWarnings("unchecked")
+    public static Collection<Change> getChanges(KnowledgeBase kb) {
+        ChangeFactory factory = new ChangeFactory(getChangesKb(kb));
+        return factory.getAllChangeObjects(true);
+    }
 
-	public static Collection<Annotation> getTopLevelAnnotationInstances(KnowledgeBase kb) {
-		Collection<Annotation> allAnnotations = getAnnotations(kb);
-		if (allAnnotations == null) {
-			return null;
-		}
+    //this should be cached in future
+    public static Collection<Annotation> getTopLevelDiscussionThreads(KnowledgeBase kb) {
+        Collection<Annotation> discussionThreadAnnotations = new ArrayList<Annotation>();
+        Collection<Annotation> allAnnotations = getAnnotations(kb);
 
-		Collection<Annotation> toRemove = new ArrayList<Annotation>();
-		for (Annotation annotInstance : allAnnotations) {
-			toRemove.addAll(annotInstance.getAssociatedAnnotations());
-		}
-		allAnnotations.removeAll(toRemove);
-		return allAnnotations;
-	}
+        for (Annotation annotation : allAnnotations) {
+            Collection annotates = annotation.getAnnotates();
+            if (annotates == null || annotates.size() == 0) {
+                discussionThreadAnnotations.add(annotation);
+            }
+        }
 
-	@SuppressWarnings("unchecked")
-	public static Collection<Annotation> getAnnotations(KnowledgeBase kb) {
-		AnnotationFactory factory = new AnnotationFactory(getChangesKb(kb));
-		return factory.getAllAnnotationObjects(true);
-	}
+        return discussionThreadAnnotations;
+    }
 
+    public static boolean hasAnnotations(Frame frame) {
+        return HasAnnotationCache.hasAnnotations(frame);
+    }
 
-	@SuppressWarnings("unchecked")
-	public static Collection<Change> getChanges(KnowledgeBase kb) {
-		ChangeFactory factory = new ChangeFactory(getChangesKb(kb));
-		return factory.getAllChangeObjects(true);
-	}
+    public static int getAnnotationCount(Frame frame) {
+        return HasAnnotationCache.getAnnotationCount(frame);
+    }
 
-	//this should be cached in future
-	public static Collection<Annotation> getTopLevelDiscussionThreads(KnowledgeBase kb) {
-		Collection<Annotation> discussionThreadAnnotations = new ArrayList<Annotation>();
-		Collection<Annotation> allAnnotations =getAnnotations(kb);
+    public static boolean isChangesOntologyPresent(KnowledgeBase kb) {
+        return getChangesKb(kb) != null;
+    }
 
-		for (Annotation annotation : allAnnotations) {
-			Collection annotates = annotation.getAnnotates();
-			if (annotates == null || annotates.size() == 0) {
-				discussionThreadAnnotations.add(annotation);
-			}
-		}
+    public static KnowledgeBase getChangesKb(KnowledgeBase kb) {
+        return ChAOKbManager.getChAOKb(kb);
+    }
 
-		return discussionThreadAnnotations;
-	}
-
-	public static boolean hasAnnotations(Frame frame) {
-		return HasAnnotationCache.hasAnnotations(frame);
-	}
-
-
-	public static boolean isChangesOntologyPresent(KnowledgeBase kb) {
-		return getChangesKb(kb) != null;
-	}
-
-	public static KnowledgeBase getChangesKb(KnowledgeBase kb) {
-		return ChAOKbManager.getChAOKb(kb);
-	}
-
-
-	private static KnowledgeBase getServerSideChangeKb(KnowledgeBase kb) {
-		String annotationName = (String) new GetAnnotationProjectName(kb).execute();
-		if (annotationName == null) {
-			Log.getLogger().warning("annotation project not configured (use " +
-					GetAnnotationProjectName.METAPROJECT_ANNOTATION_PROJECT_SLOT +
-			" slot)");
-		}
-		RemoteProjectManager project_manager = RemoteProjectManager.getInstance();
-		FrameStoreManager framestore_manager = ((DefaultKnowledgeBase) kb).getFrameStoreManager();
-		RemoteClientFrameStore remote_frame_store = framestore_manager.getFrameStoreFromClass(RemoteClientFrameStore.class);
-		RemoteServer server = remote_frame_store.getRemoteServer();
+    private static KnowledgeBase getServerSideChangeKb(KnowledgeBase kb) {
+        String annotationName = (String) new GetAnnotationProjectName(kb).execute();
+        if (annotationName == null) {
+            Log.getLogger().warning(
+                    "annotation project not configured (use "
+                            + GetAnnotationProjectName.METAPROJECT_ANNOTATION_PROJECT_SLOT + " slot)");
+        }
+        RemoteProjectManager project_manager = RemoteProjectManager.getInstance();
+        FrameStoreManager framestore_manager = ((DefaultKnowledgeBase) kb).getFrameStoreManager();
+        RemoteClientFrameStore remote_frame_store = framestore_manager
+                .getFrameStoreFromClass(RemoteClientFrameStore.class);
+        RemoteServer server = remote_frame_store.getRemoteServer();
         RemoteSession session = remote_frame_store.getSession();
         try {
             session = server.cloneSession(session);
@@ -186,101 +183,104 @@ public class ChAOUtil {
             return null;
         }
         Project changes_project = project_manager.connectToProject(server, session, annotationName);
-		return changes_project == null ? null: changes_project.getKnowledgeBase();
-	}
+        return changes_project == null ? null : changes_project.getKnowledgeBase();
+    }
 
+    public static Annotation createAnnotationOnAnnotation(KnowledgeBase kb, Frame annotatedFrame, Cls annotationType) {
+        KnowledgeBase changesKb = ChAOKbManager.getChAOKb(kb);
+        Annotation annot = OntologyJavaMappingUtil.createObject(changesKb, null, annotationType.getName(),
+                Annotation.class);
+        if (annotatedFrame == null) {
+            return annot;
+        }
+        //maybe annotatedFrame is in the changes kb
+        if (annotatedFrame.getKnowledgeBase().equals(changesKb)) { //annotated frame is an annotatable thing
+            AnnotatableThing thing = OntologyJavaMappingUtil.getSpecificObject(changesKb, (Instance) annotatedFrame,
+                    AnnotatableThing.class);
+            if (thing == null) {
+                Log.getLogger().warning("Could not find annotatable thing: " + annotatedFrame);
+                return annot;
+            }
+            thing.addAssociatedAnnotations(annot);
 
-	public static Annotation createAnnotationOnAnnotation(KnowledgeBase kb, Frame annotatedFrame, Cls annotationType) {
-		KnowledgeBase changesKb = ChAOKbManager.getChAOKb(kb);
-		Annotation annot = OntologyJavaMappingUtil.createObject(changesKb, null, annotationType.getName(), Annotation.class);
-		if (annotatedFrame == null) {
-			return annot;
-		}
-		//maybe annotatedFrame is in the changes kb
-		if (annotatedFrame.getKnowledgeBase().equals(changesKb)) { //annotated frame is an annotatable thing
-			AnnotatableThing thing = OntologyJavaMappingUtil.getSpecificObject(changesKb, (Instance) annotatedFrame, AnnotatableThing.class);
-			if (thing == null) {
-				Log.getLogger().warning("Could not find annotatable thing: " + annotatedFrame);
-				return annot;
-			}
-			thing.addAssociatedAnnotations(annot);
+        } else { //annotated frame is a domain entity
+            Ontology_Component ontologyComp = getOntologyComponent(annotatedFrame, true);
+            if (ontologyComp != null) {
+                annot.setAnnotates(CollectionUtilities.createCollection(ontologyComp));
+            }
+        }
+        return annot;
+    }
 
-		} else { //annotated frame is a domain entity
-			Ontology_Component ontologyComp = getOntologyComponent(annotatedFrame, true);
-			if (ontologyComp != null) {
-				annot.setAnnotates(CollectionUtilities.createCollection(ontologyComp));
-			}
-		}
-		return annot;
-	}
+    /**
+     * This will return a collection of all the tree nodes that have the root
+     * the collection argument that fullfill the filter condition.
+     * 
+     * @param collection
+     * @param filter
+     * @return
+     */
+    public static <X> Collection<? extends X> getFilteredCollection(Collection<? extends X> collection,
+            TreeFilter<X> filter) {
+        if (filter == null) {
+            return collection;
+        }
 
-	/**
-	 * This will return a collection of all the tree nodes that have the root the collection argument
-	 * that fullfill the filter condition.
-	 * @param collection
-	 * @param filter
-	 * @return
-	 */
-	public static <X> Collection<? extends X> getFilteredCollection(Collection<? extends X> collection, TreeFilter<X> filter) {
-		if (filter == null) {
-			return collection;
-		}
+        Collection<X> filteredCollection = new ArrayList<X>();
+        AnnotationsTreeRoot treeRoot = new AnnotationsTreeRoot(collection);
+        filteredCollection.addAll(filterNodes(treeRoot, filter));
 
-		Collection<X> filteredCollection = new ArrayList<X>();
-		AnnotationsTreeRoot treeRoot = new AnnotationsTreeRoot(collection);
-		filteredCollection.addAll(filterNodes(treeRoot, filter));
+        return filteredCollection;
+    }
 
-		return filteredCollection;
-	}
+    private static <X> Collection<X> filterNodes(LazyTreeNode node, TreeFilter<X> filter) {
+        Collection<X> filteredCollection = new HashSet<X>();
+        X userObject = (X) node.getUserObject();
 
-	private static <X> Collection<X> filterNodes(LazyTreeNode node, TreeFilter<X> filter) {
-		Collection<X> filteredCollection = new HashSet<X>();
-		X userObject = (X) node.getUserObject();
+        //userObject is a collection if the node is a root, and we don't want to add that
+        if (!(userObject instanceof Collection)) {
+            if (filter.isValid(userObject)) {
+                filteredCollection.add(userObject);
+            }
+        }
 
-		//userObject is a collection if the node is a root, and we don't want to add that
-		if (!(userObject instanceof Collection)) {
-			if (filter.isValid(userObject)) {
-				filteredCollection.add(userObject);
-			}
-		}
+        Enumeration<X> children = node.children();
+        while (children.hasMoreElements()) {
+            filteredCollection.addAll(filterNodes((LazyTreeNode) children.nextElement(), filter));
+        }
+        return filteredCollection;
+    }
 
-		Enumeration<X> children = node.children();
-		while (children.hasMoreElements()) {
-			filteredCollection.addAll(filterNodes((LazyTreeNode) children.nextElement(), filter));
-		}
-		return filteredCollection;
-	}
+    public static Collection<Ontology_Component> getOntologyComponents(KnowledgeBase kb) {
+        List<Ontology_Component> oComps = new ArrayList<Ontology_Component>();
+        KnowledgeBase changesKb = ChAOKbManager.getChAOKb(kb);
+        if (changesKb == null) {
+            return oComps;
+        }
 
-	
-	public static Collection<Ontology_Component> getOntologyComponents(KnowledgeBase kb) {
-		List<Ontology_Component> oComps = new ArrayList<Ontology_Component>();
-		KnowledgeBase changesKb = ChAOKbManager.getChAOKb(kb);
-		if (changesKb == null) { return oComps;	}
-		
-		OntologyComponentFactory factory = new OntologyComponentFactory(changesKb);
-		return factory.getAllOntology_ComponentObjects(true);
-	}
-	
-	
-	public static Collection<Ontology_Component> getOntologyComponentsWithAnnotations(KnowledgeBase kb) {
-		List<Ontology_Component> oComps = new ArrayList<Ontology_Component>();
-		KnowledgeBase changesKb = ChAOKbManager.getChAOKb(kb);
-		if (changesKb == null) { return oComps;	}		
-		OntologyComponentFactory factory = new OntologyComponentFactory(changesKb);		
-		Collection<Ontology_Component> comps = factory.getAllOntology_ComponentObjects(true);
-		for (Ontology_Component comp : comps) {
-			if (comp.hasAssociatedAnnotations()) {
-				oComps.add(comp);
-			}
-		}
-		return oComps;
-	}
+        OntologyComponentFactory factory = new OntologyComponentFactory(changesKb);
+        return factory.getAllOntology_ComponentObjects(true);
+    }
 
-	
-	public static void fillAnnotationSystemFields(KnowledgeBase kb, Annotation annotation) {
-		AnnotationFactory factory = new AnnotationFactory(getChangesKb(kb));
-		factory.fillDefaultValues(annotation);
-	}
+    public static Collection<Ontology_Component> getOntologyComponentsWithAnnotations(KnowledgeBase kb) {
+        List<Ontology_Component> oComps = new ArrayList<Ontology_Component>();
+        KnowledgeBase changesKb = ChAOKbManager.getChAOKb(kb);
+        if (changesKb == null) {
+            return oComps;
+        }
+        OntologyComponentFactory factory = new OntologyComponentFactory(changesKb);
+        Collection<Ontology_Component> comps = factory.getAllOntology_ComponentObjects(true);
+        for (Ontology_Component comp : comps) {
+            if (comp.hasAssociatedAnnotations()) {
+                oComps.add(comp);
+            }
+        }
+        return oComps;
+    }
 
+    public static void fillAnnotationSystemFields(KnowledgeBase kb, Annotation annotation) {
+        AnnotationFactory factory = new AnnotationFactory(getChangesKb(kb));
+        factory.fillDefaultValues(annotation);
+    }
 
 }
