89ebbc7ae5d0bdacd8046bd32c4cf94258071d73
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2009-12-15 22:30:23 +0000

    reinit caches if archived state changed

--- protege-collab/trunk/src/edu/stanford/smi/protege/collab/annotation/gui/ConfigureCollabProtegeAction.java
+++ protege-collab/trunk/src/edu/stanford/smi/protege/collab/annotation/gui/ConfigureCollabProtegeAction.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 
 import edu.stanford.smi.protege.collab.projectPlugin.ProtegeCollabGUIProjectPlugin;
+import edu.stanford.smi.protege.collab.util.HasAnnotationCache;
 import edu.stanford.smi.protege.model.KnowledgeBase;
 import edu.stanford.smi.protege.ui.ProjectManager;
 import edu.stanford.smi.protege.util.ModalDialog;
@@ -22,7 +23,9 @@ public class ConfigureCollabProtegeAction extends AbstractAction {
 		this.collabPlugin = collabPlugin;
 	}
 	
-	public void actionPerformed(ActionEvent arg0) {	
+	public void actionPerformed(ActionEvent arg0) {
+		boolean oldHideArchived = StatusComboBoxUtil.getHideArchived(kb.getProject());
+		
 		ConfigureCollabProtegePanel configPanel = new ConfigureCollabProtegePanel(kb.getProject());
 		int sel = ModalDialog.showDialog(ProjectManager.getProjectManager().getMainPanel(), 
 				configPanel, "Configure Collaborative Protege", ModalDialog.MODE_OK_CANCEL);
@@ -30,7 +33,13 @@ public class ConfigureCollabProtegeAction extends AbstractAction {
 		AnnotationsDisplayComponent annotationsDisplayComponent = collabPlugin.getAnnotationsDisplayComponent();
 		if (sel == ModalDialog.OPTION_OK && annotationsDisplayComponent != null) {
 			annotationsDisplayComponent.reloadCollabTabs();
-		}		
+		}
+		
+		boolean newHideArchived = StatusComboBoxUtil.getHideArchived(kb.getProject());
+		if (oldHideArchived != newHideArchived) {
+			HasAnnotationCache.fillHasAnnotationCache(kb);
+			ProjectManager.getProjectManager().getCurrentProjectView().revalidate();
+		}
 	}
 
 }
