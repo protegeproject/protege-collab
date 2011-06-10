f0c2fe0ff884a53e9dc6923fc6c14b04dfdc1b36
nyulas <2441046+csnyulas@users.noreply.github.com>
2011-06-10 07:46:29 +0000

    Added utility methods to create closures over an instance using an arbitrary property, AND methods to retrieve all values attached through a specific property to all the instances in such a closure.
    IMPORTANT: Before the next Protege release all these new methods need to be moved in the ModelUtilities.java class, and jUnit test needs to be activated.

--- src/edu/stanford/smi/protege/collab/util/KBUtil.java
+++ src/edu/stanford/smi/protege/collab/util/KBUtil.java
@@ -5,6 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -13,6 +14,7 @@ import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,6 +30,7 @@ import edu.stanford.smi.protege.model.DefaultSimpleInstance;
 import edu.stanford.smi.protege.model.FrameID;
 import edu.stanford.smi.protege.model.Instance;
 import edu.stanford.smi.protege.model.KnowledgeBase;
+import edu.stanford.smi.protege.model.Slot;
 import edu.stanford.smi.protege.model.framestore.MergingNarrowFrameStore;
 import edu.stanford.smi.protege.model.framestore.NarrowFrameStore;
 import edu.stanford.smi.protege.storage.database.AbstractDatabaseFrameDb;
@@ -415,5 +418,119 @@ public class KBUtil {
             }
         }
     }
+
+    
+    /* ******************************************************************** *
+     * TODO BEFORE THE NEXT PROTEGE RELEASE MOVE THE FOLLOWING 4 METHODS INTO 
+     * 		THE ModelUtilities CLASS!!!!!!!!!!!
+     *      AND 
+     *      ACTIVATE THE ModelUtilities_Test JUNIT TEST!!!!!!!
+     * ******************************************************************** */
+    
+    /**
+     * Returns all own slot values for the slot <code>slot</code> for every superclass of a class.
+     * 
+     * @param cls - a class
+     * @param slot - a slot
+     * 
+     * @return a map containing all own slot values as keys, and each value is mapped to a list
+     * 		of classes which contained the value as their own slot value
+     */
+    public static Map<Object, List<Instance>> getPropertyValuesOnAllSuperclasses(Cls cls, Slot slot) {
+    	KnowledgeBase kb = cls.getKnowledgeBase();
+    	Cls rootCls = kb.getRootCls();
+    	Slot parentSlot = kb.getSystemFrames().getDirectSuperclassesSlot();
+    	
+    	return getPropertyValuesOnPropertyClosureToRoot(cls, parentSlot, rootCls, slot);
+    }
+    
+    /**
+     * Returns all own slot values for the slot <code>slot</code> for every instance 
+     * in any of the paths between an instance (<code>resource</code>) and 
+     * a root instances (<code>rootResource</code>) following the relationships 
+     * defined by the <code>parentSlot</code> slot.
+     * 
+     * @param resource - a resource
+     * @param parentSlot - the slot that is used to traverse the instance graph to the <code>rootResource</code>
+     * @param rootResource - the resource that is the
+     * @param slot - a slot
+     * 
+     * @return a map containing all own slot values as keys, and each value is mapped to a list
+     * 		of instances which contained the value as their own slot value
+     */
+    public static Map<Object, List<Instance>> getPropertyValuesOnPropertyClosureToRoot(
+    		Instance resource, Slot parentSlot, Instance rootResource, Slot slot) {
+    	Map<Object, List<Instance>> result = new HashMap<Object, List<Instance>>();
+    	
+    	Collection<List<Instance>> propertyClosureToRoot = getPropertyClosureToRoot(resource, parentSlot, rootResource);
+    	Set<Instance> allNodes = new HashSet<Instance>();
+    	for (List<Instance> path : propertyClosureToRoot) {
+    		allNodes.addAll(path);
+    	}
+    	
+    	for (Instance node : allNodes) {
+    		Collection<?> values = node.getOwnSlotValues(slot);
+    		for (Object value : values) {
+    			if (value != null) {
+    				List<Instance> nodeList = result.get(value);
+    				if (nodeList == null) {
+    					nodeList = new ArrayList<Instance>();
+    					result.put(value, nodeList);
+    				}
+    				nodeList.add(node);
+    			}
+    		}
+    	}
+    	
+    	return result;
+    }
+
+    /**
+     * Computes all paths from an instance to the "root instance" node by navigating on a
+     * given slot.
+     * 
+     * @param resource - an instance
+     * @param parentSlot - slot to navigate on towards a "root node"
+     * @param rootResource - an instance considered as the root of the navigation tree, 
+     * 				necessary to stop the navigation. 
+     * 
+     * @return a collection of the paths from the resource to the root resource
+     */
+    public static Collection<List<Instance>> getPropertyClosureToRoot(Instance resource, Slot parentSlot, Instance rootResource) {
+        Collection<List<Instance>> results = new ArrayList<List<Instance>>();
+        if (resource.equals(rootResource)) {
+            results.add(Collections.singletonList(rootResource));
+            return results;
+        }
+        getPropertyClosureToRoot(resource, parentSlot, rootResource, new LinkedList<Instance>(), results);
+        return results;
+    }
+    
+    private static void getPropertyClosureToRoot(Instance resource, Slot parentSlot, Instance rootResource, 
+    		List<Instance> path, Collection<List<Instance>> pathLists) {
+        path.add(0, resource);
+
+        Collection<?> parents = resource.getOwnSlotValues(parentSlot);
+
+        for (Object parentObject : parents) {
+        	if (parentObject instanceof Instance) {
+        		Instance parentResource = (Instance) parentObject;
+	            if (parentResource.equals(rootResource)) {
+	                List<Instance> copyPathList = new ArrayList<Instance>(path);
+	                copyPathList.add(0, parentResource);
+	                pathLists.add(copyPathList);
+	            } else if (!path.contains(parentResource)) {
+	                //if (ModelUtilities.isVisibleInGUI(parentResource)) { //TODO: do we want this?
+	                List<Instance> copyPath = new ArrayList<Instance>(path);
+	                getPropertyClosureToRoot(parentResource, parentSlot, rootResource, copyPath, pathLists);
+	                //}
+	            }
+        	}
+        }
+    }
+
+    /* ******************************************************************** *
+     * END OF TODO 
+     * ******************************************************************** */
 
 }
